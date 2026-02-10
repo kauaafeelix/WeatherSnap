53f6387f8bad784b4ad68f83cd1c7f18bc9d4b7e
Kaua Felix <kfelixcosta11@gmail.com>
2026-02-10 14:40:41 -0300

    refactor: renomeia ícones e ajusta as condições climáticas para português

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ const API_KEY = "a71b657df07c1226730daac45d849043";
         const bodyApp = document.getElementById('body-app');
         const iconContainer = document.getElementById('icon-container');
 
-        const iconSun = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="text-yellow-300"><circle cx="12" cy="12" r="5"/><line x1="12" y1="1" x2="12" y2="3"/><line x1="12" y1="21" x2="12" y2="23"/><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"/><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"/><line x1="1" y1="12" x2="3" y2="12"/><line x1="21" y1="12" x2="23" y2="12"/><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"/><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"/></svg>`;
-        const iconMoon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="text-white"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>`;
+        const iconSol = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="text-yellow-300"><circle cx="12" cy="12" r="5"/><line x1="12" y1="1" x2="12" y2="3"/><line x1="12" y1="21" x2="12" y2="23"/><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"/><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"/><line x1="1" y1="12" x2="3" y2="12"/><line x1="21" y1="12" x2="23" y2="12"/><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"/><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"/></svg>`;
+        const iconLua = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="text-white"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>`;
 
         const coresClima = {
-            Clear: 'bg-orange-400',
-            Clouds: 'bg-slate-500',
-            Rain: 'bg-blue-800',
-            Thunderstorm: 'bg-zinc-900',
-            Drizzle: 'bg-cyan-700',
-            Snow: 'bg-sky-200',
+            Limpo: 'bg-orange-400',
+            Nuvens: 'bg-slate-500',
+            Chuva: 'bg-blue-800',
+            Tempestade: 'bg-zinc-900',
+            Chuvisco: 'bg-cyan-700',
+            Neve: 'bg-sky-200',
             Default: 'bg-sky-500' 
         };
 
